989c68866e14a558813aebfe6bad2e5457919f5f
alexsummers <alexsummers@yandex.ua>
2013-12-02 15:42:09 +0200

    BDD start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HelperCloud::Application.config.secret_key_base = '7e870db9f65ef6667392c293afa6e5376628a29d63b4b9a0f2aa15f210d9e4a614670ccb9ea337fcdea5887b725d751d58a5092b6430d2e14dc36a5a62711936'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HelperCloud::Application.config.session_store :cookie_store, key: '_helper_cloud_session'
